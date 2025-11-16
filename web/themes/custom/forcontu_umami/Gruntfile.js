e56ca2d28ca87cfdb2cdbcd155eebf45f1e3efd9
Álvaro Olvera Fernández <aolvera@externas.jccm.es>
2025-11-16 20:37:36 +0100

    pec1

---
+++ web/themes/custom/forcontu_umami/Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'scss',
+          cssDir: 'css',
+          sourcemap: true
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['compass']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default',['watch']);
+}
